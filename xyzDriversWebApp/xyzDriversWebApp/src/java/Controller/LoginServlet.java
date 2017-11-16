1c85cb0b546eabe3daeb76eac4c28650bad3c8ff
sashag211 <33229326+sashag211@users.noreply.github.com>
2017-11-16 19:49:56 +0000

    More changes made to project file structure with empty files

--- xyzDriversWebApp/xyzDriversWebApp/src/java/Controller/LoginServlet.java
+++ xyzDriversWebApp/xyzDriversWebApp/src/java/Controller/LoginServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com;
+package Controller;
 
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
