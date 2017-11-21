cb65c7c53849bbd6cce1e6ae92a6ba238ecadc86
frazer19 <frazerpinheiro@hotmail.co.uk>
2017-11-21 16:34:00 +0000

    Add files via upload

---
+++ xyzDriversWebApp/xyzDriversWebApp/src/java/model/DBConnection.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author f2-pinheiro
+ */
+public class DBConnection {
+    
+    public static final String HOST = "jdbc:derby://localhost:1527/XYZDriver";
+    public static final String USER = "Root";
+    public static final String PASS = "1234";
+    
+}
---
+++ xyzDriversWebApp/xyzDriversWebApp/src/java/model/User.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author f2-pinheiro
+ */
+public class User {
+    String id;
+    String password;
+    String status;
+    
+    public User(String id, String password, String status){
+        this.id = id;
+        this.password = password;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
+
